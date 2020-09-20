9b3446c05232ff7a4596b538a072380776cd755c
katarinatodorovic <kayattd@hotmail.com>
2020-09-20 16:49:19 +0200

    Organization

--- OrthodoxEaster.java
+++ OrthodoxEaster.java
@@ -17,11 +17,11 @@ public class OrthodoxEaster{
         int c = godina % 19;
         int d = (19 * c + 15) % 30;
         int e = (2 * a + 4 * b - d + 34) % 7;
-        int month = (d + e + 114) / 31;
-        int day = ((d + e + 114) % 31) + 1;
-        int dayPoNovom = day + 13;
+        int mesecBr = (d + e + 114) / 31;
+        int dan = ((d + e + 114) % 31) + 1;
+        int danPoNovom = dan + 13;
         String mesec = "";
-        switch (month) {
+        switch (mesecBr) {
             case 3:
                 mesec = "marta";
                 break;
@@ -34,22 +34,22 @@ public class OrthodoxEaster{
 
         if (godina >= 1900 && godina <= 1919) {
 
-            System.out.println("Uskrs je bio " + day + ", " + mesec + " po starom kalendaru");
+            System.out.println("Uskrs je bio " + dan + ", " + mesec + " po starom kalendaru");
 
         }
         if (godina > 1919) {
 
-            if (month == 3 && dayPoNovom > 31) {
+            if (mesecBr == 3 && danPoNovom > 31) {
                 mesec = "aprila";
-                dayPoNovom = dayPoNovom - 31;
+                danPoNovom = danPoNovom - 31;
 
             }
 
-            if (month == 4 && dayPoNovom > 30) {
+            if (mesecBr == 4 && danPoNovom > 30) {
                 mesec = "maja";
-                dayPoNovom = dayPoNovom - 30;
+                danPoNovom = danPoNovom - 30;
             }
-            System.out.println("Uskrs je padao " + dayPoNovom + " " + mesec + " po novom kalendaru");
+            System.out.println("Uskrs je padao " + danPoNovom + " " + mesec + " po novom kalendaru");
         }
     }
 }
